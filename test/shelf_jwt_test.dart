1214ce13b993251a4ce05d25bbe5418dfecffcf2
parkeradam <30392071+parkeradam@users.noreply.github.com>
2022-05-17 23:44:18 +0100

    New dart package template

---
+++ test/shelf_jwt_test.dart
@@ -0,0 +1,16 @@
+import 'package:shelf_jwt/shelf_jwt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
